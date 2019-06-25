fa1bdca19f6e033d11dc6a68dddabd64e3e32cb5
Daniel Schneider <daschne8@gmail.com>
2019-06-25 14:34:07 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,6 +5,9 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
-    @resp = Faraday.get 'https://api.github.com/search/repositories'
+    @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
+      req.params['client_id'] = Iv1.5145830fef3e3784
+      req.params['client_secret'] = Iv1.5145830fef3e3784
+    end
   end
 end
