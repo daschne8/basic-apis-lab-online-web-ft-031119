2f33135c5dbcd13e53a8c5893ef7268dee247404
Daniel Schneider <daschne8@gmail.com>
2019-06-25 14:43:36 +0000

    Automatically backed up by Learn

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,8 @@ module BasicApisLab
     # -- all .rb files in that directory are automatically loaded.
   end
 end
-
+# Version of your assets, change this if you want to expire all your assets
+    config.assets.version = '1.0'
 config.before_configuration do
   env_file = File.join(Rails.root, 'config', 'local_env.yml')
   YAML.load(File.open(env_file)).each do |key, value|
