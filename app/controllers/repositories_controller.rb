f397dc7a1d44a2f1a1125d1a72602ee78e803548
Daniel Schneider <daschne8@gmail.com>
2019-06-25 14:49:42 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -8,6 +8,7 @@ class RepositoriesController < ApplicationController
     @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
       req.params['client_id'] = ENV['CLIENT_ID']
       req.params['client_secret'] = ENV['CLIENT_SECRET']
+      req.params['q'] = 'tetris'
     end
   end
 end
